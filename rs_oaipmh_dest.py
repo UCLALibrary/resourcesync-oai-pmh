d7843a43b4fe109b693535eea210ac8361fe2e23
Mark Allen Matney, Jr <mmatney@library.ucla.edu>
2017-08-25 15:09:30 -0700

    refactor, add comments and documentation

--- rs_oaipmh_dest.py
+++ rs_oaipmh_dest.py
@@ -7,26 +7,27 @@
 # - Synchronize our local copy of each collection with updates from member institutions.
 # - Propagate these changes to Solr
 
-import os
-import pdb
-import re
-import pysolr
-import collections
-import subprocess
-from tinydb import TinyDB, Query
-from bs4 import BeautifulSoup
 import argparse
-import logging
-import sys
-from dateutil.parser import parse
+from bs4 import BeautifulSoup
+import collections
 from datetime import date
-import validators
-from requests import get
-from json import dumps
+from dateutil.parser import parse
 from functools import reduce
+from json import dumps
+import logging
+import os
+import pysolr
+import re
+from requests import get
+import subprocess
+import sys
+from tinydb import TinyDB, Query
+import validators
 
+# URL of the default "No thumbnail available" thumbnail
 noThumbnailExistsUrl = 'http://nothumb.com'
 
+# map from DC tag name to Solr field name
 tagNameToColumn = {
     'title': 'title_keyword',
     'creator': 'creator_keyword',
@@ -45,37 +46,69 @@ tagNameToColumn = {
     'rights': 'rights_keyword'
     }
 
+# list of BeautifulSoup filters to pass to the find_all function
 bsFilters = [
     'identifier',
     'identifier.thumbnail',
     re.compile('identifier.*')
     ]
 
-### regular expressions ###
+# regular expressions used for matching non-standard date formats
+regexes = {
+    'match': {},
+    'substitution': {}
+    }
+regexes['match']['mm'] = r'(?:0[1-9]|1[0-2])'
+regexes['match']['dd'] = r'(?:0[1-9]|[1-2]\d|3[1-2])'
+regexes['match']['mon'] = r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
+regexes['match']['time'] = r'\d{1,2}[.:]\d{2}(?:[ap]\.?m)?'
+regexes['match']['year'] = r'[1-9]\d*'
+regexes['match']['year?'] = r'[1-9]\d*[-*?]?'
+regexes['match']['year-mm'] = r'{}(?:(?:[-/]{})|[-*?])?(?=\D|$)'.format(
+    regexes['match']['year'],
+    regexes['match']['mm'])
 
-# 1. used for cleanAndNormalizeDate
-twoDigitMonth = r'(?:0[1-9]|1[0-2])'
-twoDigitDay = r'(?:0[1-9]|[1-2]\d|3[1-2])'
-threeCharMonth = r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
-timePattern = r'\d{1,2}[.:]\d{2}(?:[ap]\.?m)?'
-yearPattern = r'[1-9]\d*'
-yearPatternMissingOnes = r'[1-9]\d*[-*?]?'
-yearMonthPattern = r'{}(?:(?:[-/]{})|[-*?])?(?=\D|$)'.format(yearPattern, twoDigitMonth)
-yearRangePattern = r'{}\s*[-/]\s*{}'.format(yearMonthPattern, yearMonthPattern)
-dmyt = r'{}\s+{}\s+{}(?:\.\s+{})?'.format(twoDigitDay, threeCharMonth, yearPattern, timePattern)
-consecutiveYearsPattern = r'{}/\d'.format(yearPattern)
-centuryPattern = r'(?:1st|2nd|3rd|(?:[4-9]|1[0-9]|20)th)\s+[cC](?:entury)?'
+regexes['match']['year-year'] = r'{}\s*[-/]\s*{}'.format(
+    regexes['match']['year-mm'],
+    regexes['match']['year-mm'])
 
-# the order of checks is important (try to match yeraRangePattern before yearPatternMissingOnes
-datePattern = r'(?:({})|({})|({})|({})|({}))'.format(centuryPattern, yearRangePattern, dmyt, consecutiveYearsPattern, yearPatternMissingOnes)
-suffixBeforeYear0 = r'BC|B\.C\.|BCE|B\.C\.E\.'
-suffixAfterYear0 = r'AD|A\.D\.|CE|C\.E\.'
-suffixPattern = r'(?:{}|{})'.format(suffixBeforeYear0, suffixAfterYear0)
-finalPattern = r'{}(?:\s+({}))?'.format(datePattern, suffixPattern)
+regexes['match']['dd-mon-year-time'] = r'{}\s+{}\s+{}(?:\.\s+{})?'.format(
+    regexes['match']['dd'],
+    regexes['match']['mon'],
+    regexes['match']['year'],
+    regexes['match']['time'])
 
-# 2. used for dateMatchToInt
-yearRangeCaptureYearsPattern = r'({})\s*[-/]\s*({})'.format(yearMonthPattern, yearMonthPattern)
-dmytCaptureYearPattern = r'{}\s+{}\s+({})(?:\.\s+{})?'.format(twoDigitDay, threeCharMonth, yearPattern, timePattern)
+regexes['match']['year/m'] = r'{}/\d'.format(regexes['match']['year'])
+regexes['match']['century'] = r'(?:1st|2nd|3rd|(?:[4-9]|1[0-9]|20)th)\s+[cC](?:entury)?'
+
+# order of alternate patterns is important
+regexes['match']['alternator'] = r'(?:({})|({})|({})|({})|({}))'.format(
+    regexes['match']['century'],
+    regexes['match']['year-year'],
+    regexes['match']['dd-mon-year-time'],
+    regexes['match']['year/m'],
+    regexes['match']['year?'])
+
+regexes['match']['suffix-bce'] = r'BC|B\.C\.|BCE|B\.C\.E\.'
+regexes['match']['suffix-ce']= r'AD|A\.D\.|CE|C\.E\.'
+regexes['match']['suffix'] = r'(?:{}|{})'.format(
+    regexes['match']['suffix-bce'],
+    regexes['match']['suffix-ce'])
+
+regexes['match']['date'] = r'{}(?:\s+({}))?'.format(
+    regexes['match']['alternator'],
+    regexes['match']['suffix'])
+
+regexes['substitution']['year-year'] = r'({})\s*[-/]\s*({})'.format(
+    regexes['match']['year-mm'],
+    regexes['match']['year-mm'])
+
+regexes['substitution']['dd-mon-year-time'] = r'{}\s+{}\s+({})(?:\.\s+{})?'.format(
+    regexes['match']['dd'],
+    regexes['match']['mon'],
+    regexes['match']['year'],
+    regexes['match']['time'])
+
 
 def addValuePossiblyDuplicateKey(key, value, dic):
     '''Adds a key-value pair to a dictionary that may already have a value for that key. If that's the case, put both values into a list. This is how pysolr wants us to represent duplicate fields.'''
@@ -88,63 +121,66 @@ def addValuePossiblyDuplicateKey(key, value, dic):
     else:
         dic[key] = value
 
+
 def createSolrDoc(identifier, colname, instname, thumbnailurl, tags):
     '''Maps a Dublin Core record to a Solr document to be indexed.'''
 
     doc = {
         'id': identifier,
-        #'thumbnail_url': thumbnailurl,
+        'thumbnail_url': thumbnailurl,
         'collectionName': colname,
         'institutionName': instname
     }
-    dates = set()
+    years = set()
 
     for tag in tags:
 
-        # ignore newlines and other whitespace
+        # ignore newlines and other whitespace in the list of tags
         if tag.name is None:
             continue
 
+        # only process Dublin Core fields
         try:
             name = tagNameToColumn[tag.name]
         except KeyError as e:
-            # only process Dublin Core fields
             continue
+        else:
+            value = tag.string
+            addValuePossiblyDuplicateKey(name, value, doc)
 
-        value = tag.string
+            # build up a set of all the years included in the metadata
+            if name == tagNameToColumn['date']:
+                years = years | cleanAndNormalizeDate(value)
 
-        addValuePossiblyDuplicateKey(name, value, doc)
-
-        if tag.name == 'date':
-            dates = dates | cleanAndNormalizeDate(value)
-
-    decades = facet_decades(dates)
-
-    logging.debug('Dates: {}'.format(dates))
-    logging.debug('Decades: {}'.format(decades))
-
+    decades = facet_decades(years)
     for decade in decades:
         addValuePossiblyDuplicateKey('decade', decade, doc)
 
+    logging.debug('Dates: {}'.format(years))
+    logging.debug('Decades: {}'.format(decades))
     logging.debug(dumps(doc, indent=4))
+
     return doc
 
+
 def facet_decades(years):
-    '''Returns a set of decades that spans all of the years in "years".'''
+    '''Returns a set of decades that spans all of the years in the input set.'''
 
     currentYear = date.today().year
-
-    # matches = set(filter(lambda a: a >= 1000, years))
     matches = set(filter(lambda a: a <= currentYear, years))
     if len(matches) == 0:
         return set()
 
+    # round min down to nearest decade
     start = min(matches) // 10 * 10
+    # add 1 just in case max ends with 0
     end = max(matches) + 1
     return set(range(start, end, 10))
 
+
 def resolveUnknownOnes(i):
-    '''i is a string that represents a year with a possibly missing ones value.'''
+    '''i is a string that represents a year with a possibly missing ones value, like "199-?" or "199?". Round down to the nearest decade'''
+
     m = re.compile('(\d{4})').match(i)
     if m is not None:
         return int(m.group(1))
@@ -152,37 +188,43 @@ def resolveUnknownOnes(i):
         m = re.compile('(\d{1,3})[-*?]').match(i)
         return i if m is None else int(m.group(1) + '0')
 
+
 def dateMatchToInt(m):
+    '''Maps a match of regexes['match']['date'] to a set of years.'''
+
+    years = set()
+    '''
+    Match indices:
+    0 -> 'century'
+    1 -> 'year-year'
+    2 -> 'dd-mon-year-time'
+    3 -> 'year/m'
+    4 -> 'year?'
+    5 -> 'suffix'
+    '''
     try:
         if m[0] != '':
-            # century
             century = int(re.match(re.compile('\d+'), m[0]).group(0))
             years = {100 * (century - 1), 100 * (century - 1) + 99}
         elif m[1] != '':
-            # year range
-            years = {int(resolveUnknownOnes(y.strip())) for y in re.sub(yearRangeCaptureYearsPattern, r'\1>|<\2', m[1]).split('>|<')}
+            years = {int(resolveUnknownOnes(y.strip())) for y in re.sub(regexes['substitution']['year-year'], r'\1>|<\2', m[1]).split('>|<')}
         elif m[2] != '':
-            # dmyt
-            years = {int(resolveUnknownOnes(re.sub(dmytCaptureYearPattern, r'\1', m[2]).strip()))}
+            years = {int(resolveUnknownOnes(re.sub(regexes['substitution']['dd-mon-year-time'], r'\1', m[2]).strip()))}
         elif m[3] != '':
-            # consecutive years
-            # TODO: remove
+            # TODO: remove this case, we aren't handling it
             a = m[3].split('/')
             prefix = a[0][:-1]
             years = reduce(lambda x, y: x | {int('{}{}'.format(prefix, y))}, a[1:], {int(a[0])})
         elif m[4] != '':
-            # year
             years = {int(resolveUnknownOnes(m[4]))}
         else:
             # error
             raise Error
     except ValueError as e:
-        logging.error(e)
+        logging.error('An error occurred while trying to match "{}": {}'.format(m, e))
 
-
-    if m[5] != '' and re.compile(suffixBeforeYear0).match(m[5]) is not None:
-        # convert years to negatives
-        # if century notation, go further negative
+    if m[5] != '' and re.compile(regexes['match']['suffix-bce']).match(m[5]) is not None:
+        # move everything to the left side of year 0 on the timeline
         if m[0] != '':
             years = {100 * -century, 100 * -century + 99}
         else:
@@ -191,31 +233,27 @@ def dateMatchToInt(m):
     logging.debug('Mapping match to years: {} -> {}'.format(m, years))
     return years
 
-def cleanAndNormalizeDate(dateString):
-    '''Returns a normalized set of years found in the dateString.'''
 
-    # TODO: use this array to generate regex pattern
-    digitPlaceholders = [
-        '-',
-        '?',
-        '*'
-        ]
-    # First, see if dateutil can parse the date string
+def cleanAndNormalizeDate(dateString):
+    '''Returns a normalized set of years (integers) found in the input string.'''
+
+    # first see if dateutil can parse the date string
     try:
         return {parse(dateString).year}
 
     except ValueError:
-        # If not, find as many substrings that look like years as possible
         try:
             # strip alphabetical chars and spaces from the left side and try again
             return {parse(dateString.lstrip('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')).year}
         except ValueError:
-            matches = re.findall(re.compile(finalPattern), dateString)
+            # find as many substrings that look like years as possible
+            matches = re.findall(re.compile(regexes['match']['date']), dateString)
             logging.debug('{} date string matches found in "{}"'.format(len(matches), dateString))
             if len(matches) > 0:
                 return reduce(lambda x, y: x | y, [dateMatchToInt(m) for m in matches], set())
             else:
                 return set()
+
 
 def findThumbnailUrl(bs, filters):
     '''Return the URL of the thumbnail for a Dublin Core record. If none exists, return None.
@@ -226,7 +264,7 @@ def findThumbnailUrl(bs, filters):
 
     for f in filters:
 
-        # search for tags that match the filter (can be regex or string, see https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all)
+        # search for tags that match the filter (can be regex or string, see )
         tags = bs.find_all(f)
 
         for tag in tags:
@@ -240,8 +278,10 @@ def findThumbnailUrl(bs, filters):
                     return possibleUrl
     return None
 
+
 def getThumbnail(url):
     '''Puts the thumbnail file in its place on the image server, and returns its URL.'''
+
     # TODO: Put thumbnail file somewhere to be served up
     # https://stackoverflow.com/a/16696317
     thumbpath = '~/thumbnails/'
@@ -253,10 +293,10 @@ def getThumbnail(url):
             if chunk:
                 f.write(chunk)
 
-    # TODO: upload to S3?
-    # use boto
+    # TODO: upload to S3 with boto
 
     return 'http://example.com/{}'.format(filename)
+
 
 def main():
 
